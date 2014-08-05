d4287f7c61f3cdc7df8724c4cbb07d4879c4f00d
notyy <notyycn@gmail.com>
2014-08-05 21:48:39 +0800

    fighting

--- hadoop2.4/cluster/debug.sh
+++ hadoop2.4/cluster/debug.sh
@@ -1,6 +1,8 @@
 docker run -ti --name=namenode --link datanode1:datanode1 --link datanode2:datanode2 --link datanode3:datanode3 -p 50070:50070 notyy/hadoop-master:2.4.1 /etc/bootstrap.sh -bash
 
-docker run --name=namenode -d -p 50070:50070 notyy/hadoop-master:2.4.1
+docker run --name=namenode -d -h=namenode -p 50070:50070 notyy/hadoop-master:2.4.1
+
+docker run --name=namenode -ti -h=namenode -p 50070:50070 notyy/hadoop-master:2.4.1 /etc/bootstrap.sh -bash
 
 docker run --name=datanode1 -d --link namenode:namenode notyy/hadoop-slave:2.4.1
 
